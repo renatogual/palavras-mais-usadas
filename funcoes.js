12e4908b01d1808d98edcb21658d0e4d6a9985f9
eox-tecnologia <eox-tecnologia>
2020-12-01 13:49:58 -0300

    Criando funcionalidades para processar o arquivo

--- funcoes.js
+++ funcoes.js
@@ -32,9 +32,48 @@ function lerArquivos(caminhos) {
     return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
 }
 
+function limparLinhasVazias(array) {
+    return array.filter(el => el.trim())
+}
+
+function limparLinhasComPadrao(array, padrao) {
+    return array.filter(el => !el.includes(padrao))
+}
+
+function limparLinhasApenasNumeros(array) {
+    return array.filter(el => {
+        const num = parseInt(el.trim())
+        return num !== num // Irá retornar apenas o que não for números, pois NaN !== NaN retorna true
+    })
+}
+
+function limparSimbolos(array, simbolos) {
+    return array.map(el => {
+        let textoSemSimbolos = el
+        simbolos.forEach(simbolo => {
+            textoSemSimbolos = textoSemSimbolos.split(simbolo).join('')
+        });
+        return textoSemSimbolos
+    })
+}
+
+function mesclarElementos(array) {
+    return array.join(' ')
+} 
+
+function separarTextoPor(texto, simbolo) {
+    return texto.split(simbolo)
+}
+
 module.exports = {
     lerDiretorio,
     filtroExtensoes,
     lerArquivo,
-    lerArquivos
+    lerArquivos,
+    limparLinhasVazias,
+    limparLinhasComPadrao,
+    limparLinhasApenasNumeros,
+    limparSimbolos,
+    mesclarElementos,
+    separarTextoPor
 }
